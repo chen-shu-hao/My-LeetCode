58ed650c3abf5fdb39ceb453beaace31375ed24c
shuhao.chen <shuhao.chen@idreamsky.com>
2022-04-12 17:50:57 +0800

    1

--- src/leetcode/editor/cn/TreeNode.java
+++ src/leetcode/editor/cn/TreeNode.java
@@ -11,7 +11,7 @@ package leetcode.editor.cn;/**
  * 项目名称: My-LeetCode
  * @ClassName TreeNode
  * @Description
- * @Author shuhao.chen shuhao.chen@idreamsky.com   @Date 2022/3/22 14:58
+ * @Author shuhao.chen    @Date 2022/3/22 14:58
  */
 public class TreeNode {
      int val;
